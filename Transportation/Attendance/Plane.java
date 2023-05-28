8fec4d657b0321bc8fa3542e13064ccaabc77e24
Sean Kervin Vidal Gumarao <111546618+Nue1234@users.noreply.github.com>
2023-05-28 14:35:26 +0800

    Create Plane.java

---
+++ Transportation/Attendance/Plane.java
@@ -0,0 +1,16 @@
+class Plane extends Vehicle {
+    private double wingspan;
+
+    public Plane(int speed, String color, double price, double wingspan) {
+        super(speed, color, price);
+        this.wingspan = wingspan;
+    }
+
+    public void fly() { // This is a method named fly responsible for printing the message telling that the plane is flying.
+        System.out.println("The plane is flying."); 
+    }
+
+    public double getWingspan() { 
+        return wingspan; // This line returns the value of the wingspan variable when the getWingspan() method is called.
+    }
+}
